c06ab2eb03b2cfb50a4953cced37e537812cd1af
iamvysh <vyshnavthaithottathil@gmail.com>
2024-08-01 13:57:46 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,97 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  Image,
+  ImageBackground,
+  Modal,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import PokemonCard from './components/PokemonCard';
+
+function App() {
+
+
+  const charmanderData = {
+    name: "Charmander",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  };
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
+
+
+  return (
+  // <SafeAreaView style={styles.container}>
+<ScrollView>
+
+    <PokemonCard {...charmanderData}/>
+    <PokemonCard {...squirtleData}/>
+    <PokemonCard {...bulbasaurData}/>
+    <PokemonCard {...pikachuData}/>
+    <PokemonCard {...charmanderData}/>  
+    
+    </ScrollView>
+  // </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingTop:Platform.OS==='android'?25:0
+  },
+});
+
+export default App;
